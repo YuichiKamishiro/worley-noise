c0850b83146b6814bad097af1fa6269a96b6569f
YuichiKamishiro <yuichikamishiro@gmail.com>
2024-07-28 23:18:12 +0300

    white noise

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <thread>
 
 using namespace sf;
 
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
 int main() {
-    RenderWindow window(VideoMode(1280, 720), "Window");
+    srand(time(0));
+    RenderWindow window(VideoMode(WIDTH, HEIGHT), "Window");
 
     while(window.isOpen()) {
         Event event;
@@ -16,6 +22,23 @@ int main() {
 
         window.clear();
 
+        Image img;
+        img.create(WIDTH, HEIGHT, Color::Black);
+
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                int r = rand() % 2 + 0;
+                img.setPixel(j, i, Color(r * 255, r * 255, r * 255));
+            }
+        }
+        Texture texture;
+        texture.loadFromImage(img);
+        Sprite sprite(texture);
+
+
+        window.draw(sprite);        
+
         window.display();
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
     }  
 }
